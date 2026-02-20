dae7169347446469cde12119e2e7459ffcb8f8dd
installer <installer@gmail.com>
2026-02-20 17:16:14 +0100

    Removed old images upload files

--- backend/routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { asyncHandler } = require('../middleware/asyncHandler');
-const { deleteImage } = require('../controllers/imageController');
-
-// DELETE /api/images/:id
-router.delete('/:id', asyncHandler(deleteImage));
-
-module.exports = router;
